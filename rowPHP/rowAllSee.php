ed4187362b5e15a47f576d8bc247038781dc0e52
Uğur KILCI <1714709016@ogr.mehmetakif.edu.tr>
2023-10-03 09:40:48 +0300

    Create rowAllSee.php

---
+++ rowPHP/rowAllSee.php
@@ -0,0 +1,33 @@
+<?php
+
+// echo rowAllSee(
+//     "categories",
+//     ["id" => $id]
+// );
+
+function rowAllSee($tableName, $columns)
+{
+    global $db; // Global veritabanı bağlantısını kullanacağımızı belirtiyoruz.
+
+    // Sorgu için sütunları ve değerleri oluştur
+    $conditions = [];
+    $values = [];
+
+    // Gelen $columns dizisini döngüye alarak sütun adlarını ve değerlerini ayırıyoruz.
+    foreach ($columns as $column => $value) {
+        $conditions[] = "$column=?";
+        $values[] = $value;
+    }
+
+    // WHERE koşullarını oluşturuyoruz.
+    $conditions = implode(" AND ", $conditions);
+
+    // SQL sorgusunu hazırlıyoruz.
+    $see = $db->prepare("SELECT * FROM $tableName WHERE $conditions");
+    $see->execute($values);
+
+    // Sorguyu çalıştırıyoruz ve sonucu alıyoruz.
+    $result = $see->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+}
